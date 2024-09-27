615c83cb77a946f8396d91273327620a0ef9281e
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-09-27 08:07:29 +0700

    Setup project dan install dependensi yang dibutuhkan

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-cyan-900 text-white font-bold justify-center text-center m-10">
+        Hallo Dunia yang tidak baik-baik saja
+      </h1>
+    </>
+  );
+}
+
+export default App;
