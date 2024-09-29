959113b810e762088b97ad2c3f0a8db9c84768d1
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-09-30 06:00:24 +0700

    Menambahkan handlesubmit login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        { email, password }
+      );
+      if (response.data.success) {
+        login(response.data.user);
+        // console.log(response.data.user);
+        localStorage.setItem("token", response.data.token);
+        if (response.data.user.role === "admin") {
+          navigate("/admin-dashboard");
+        } else {
+          navigate("/employee-dashboard");
+        }
+        alert("Successfully login");
+      }
+      //   console.log(response);
+    } catch (error) {
+      if (error.response && !error.response.data.success) {
+        setError(error.response.data.error);
+      } else {
+        setError("Server Error");
+      }
+      //   console.log(error);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center h-screen justify-center bg-gray-500 space-y-6">
       <h2 className="text-3xl font-poppins font-extrabold">
@@ -8,7 +46,8 @@ const Login = () => {
       </h2>
       <div className="border shadow p-6 w-80 bg-gray-200">
         <h2 className="text-2xl font-bold mb-4">Login</h2>
-        <form>
+        {error && <p className="text-red-500">{error}</p>}
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700">
               Email
@@ -17,6 +56,8 @@ const Login = () => {
               type="email"
               className="w-full px-3 py-2 border"
               placeholder="Enter Email"
+              onChange={(e) => setEmail(e.target.value)}
+              required
             />
           </div>
           <div>
@@ -27,6 +68,8 @@ const Login = () => {
               type="password"
               className="w-full px-3 py-2 border"
               placeholder="*********"
+              onChange={(e) => setPassword(e.target.value)}
+              required
             />
           </div>
           <div className="mb-4 flex items-center justify-between">
