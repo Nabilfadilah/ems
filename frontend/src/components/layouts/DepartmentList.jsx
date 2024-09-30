8a6aa563abb5ef7a5c160a130036fdc76e0622d8
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-09-30 15:02:43 +0700

    Membuat layout department list

---
+++ frontend/src/components/layouts/DepartmentList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Typography from "../elements/text/Typography";
+
+const DepartmentList = () => {
+  return (
+    <div>
+      <Typography>Defape lsisss</Typography>
+    </div>
+  );
+};
+
+export default DepartmentList;
