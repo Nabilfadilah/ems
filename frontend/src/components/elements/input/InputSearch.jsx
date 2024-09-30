d8f8c7e92da1fdcdbbd51e314734bd4b6a4bf239
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-09-30 20:55:21 +0700

    Membuat element input search

---
+++ frontend/src/components/elements/input/InputSearch.jsx
@@ -0,0 +1,25 @@
+import { forwardRef } from "react";
+import { AiOutlineSearch } from "react-icons/ai"; // Import ikon dari React Icons
+
+const InputSeacrh = forwardRef((props, ref) => {
+  const { placeholder, name, className } = props;
+
+  return (
+    <div className="relative w-80">
+      {/* Ikon Search */}
+      <AiOutlineSearch className="absolute left-3 top-2.5 text-gray-400" />
+
+      {/* Input Field */}
+      <input
+        type="search"
+        className={`text-sm border border-gray-500 rounded w-full py-2 px-10 text-slate-700 placeholder:opacity-90 ${className}`}
+        placeholder={placeholder || "Cari..."}
+        name={name}
+        id={name}
+        ref={ref}
+      />
+    </div>
+  );
+});
+
+export default InputSeacrh;
