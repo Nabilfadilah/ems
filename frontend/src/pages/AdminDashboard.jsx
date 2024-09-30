2af04d12be353f1939ddca2f30deb416f75b89a8
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-09-30 15:07:06 +0700

    Memanggil sidebar admin, navbar dan outlet (admin summary)

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { useAuth } from "../context/AuthContext";
+import AdminSidebar from "../components/layouts/AdminSidebar";
+import Navbar from "../components/layouts/Navbar";
+// import AdminSummary from "../components/layouts/AdminSummary";
+import { Outlet } from "react-router-dom";
 
 const AdminDashboard = () => {
   const { user } = useAuth;
 
   return (
-    <div>
-      Admin Dashboard yaa
-      {user && user.name}
+    <div className="flex">
+      <AdminSidebar />
+      <div className="flex-1 ml-64 bg-gray-100 h-screen">
+        <Navbar />
+        {/* <AdminSummary /> */}
+        <Outlet />
+      </div>
     </div>
   );
 };
