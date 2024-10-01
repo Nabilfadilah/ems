61288cecf0364da6b52099f53454c5ce02c7f1b7
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-01 10:30:20 +0700

    Menambahkan middleware department

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 import authRouter from "./routes/auth.js"
+import departmentRoute from "./routes/department.js"
+
 import connectToDatabase from "./db/db.js"
 
 connectToDatabase()
@@ -9,6 +11,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', authRouter)
+app.use('/api/department', departmentRoute)
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
