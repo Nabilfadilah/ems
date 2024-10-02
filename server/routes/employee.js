0cb905095f568129c39226a35c6ce33b1b053f2b
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-03 06:55:23 +0700

    Menambahkan route employee untuk get id view

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -1,12 +1,12 @@
 import express from "express"
 import AuthMiddleware from "../middleware/AuthMiddleware.js"
-import { addEmployee, getEmployees, upload } from "../controllers/EmployeeController.js"
+import { addEmployee, getEmployee, getEmployees, upload } from "../controllers/EmployeeController.js"
 
 const router = express.Router()
 
 router.get('/', AuthMiddleware, getEmployees)
 router.post('/add', AuthMiddleware, upload.single('image') ,addEmployee)
-// router.get('/:id', AuthMiddleware, getDepartment)
+router.get('/:id', AuthMiddleware, getEmployee)
 // router.put('/:id', AuthMiddleware, updateDepartment)
 // router.delete('/:id', AuthMiddleware, deleteDepartment)
 
