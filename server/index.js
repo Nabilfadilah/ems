26f42dd6cc13f5d571819440c6bb82ed7d0253df
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-02 15:53:31 +0700

    Menambahkan middleware employee

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import express from "express"
 import cors from "cors"
 import authRouter from "./routes/auth.js"
-import departmentRoute from "./routes/department.js"
+import departmentRouter from "./routes/department.js"
+import employeeRouter from "./routes/employee.js"
 
 import connectToDatabase from "./db/db.js"
 
@@ -11,7 +12,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', authRouter)
-app.use('/api/department', departmentRoute)
+app.use('/api/department', departmentRouter)
+app.use('/api/employee', employeeRouter)
 
 
 app.listen(process.env.PORT, () => {
