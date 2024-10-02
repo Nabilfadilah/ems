29474ec6247ffa26b4c2e7f05584d85cc401e87f
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-02 18:43:52 +0700

    Menambahkan middleware employee untuk gambar

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// untuk gambar
+app.use(express.static('public/uploads'))
+
 app.use('/api/auth', authRouter)
 app.use('/api/department', departmentRouter)
 app.use('/api/employee', employeeRouter)
