a31ba92b283a7f23a837b100ea5e4f342241f9a9
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-02 18:43:13 +0700

    Menambahkan route employee untuk get

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -1,10 +1,10 @@
 import express from "express"
 import AuthMiddleware from "../middleware/AuthMiddleware.js"
-import { addEmployee, upload } from "../controllers/EmployeeController.js"
+import { addEmployee, getEmployees, upload } from "../controllers/EmployeeController.js"
 
 const router = express.Router()
 
-// router.get('/', AuthMiddleware, getDepartments)
+router.get('/', AuthMiddleware, getEmployees)
 router.post('/add', AuthMiddleware, upload.single('image') ,addEmployee)
 // router.get('/:id', AuthMiddleware, getDepartment)
 // router.put('/:id', AuthMiddleware, updateDepartment)
