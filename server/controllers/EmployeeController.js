b4825d03269b3da2f64cba13f403a8eaff884217
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-03 08:27:16 +0700

    Menambahkan controller employee put untuk edit

--- server/controllers/EmployeeController.js
+++ server/controllers/EmployeeController.js
@@ -3,6 +3,7 @@ import Employee from "../models/Employee.js"
 import User from "../models/User.js"
 import bcrypt from "bcrypt"
 import path from "path"
+import Department from "../models/Department.js"
 
 // upload image
 const storage = multer.diskStorage({
@@ -90,4 +91,45 @@ const getEmployee = async (req, res) => {
     }
 }
 
-export {addEmployee, upload, getEmployees, getEmployee}
+// edit
+const updateEmployee = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {name, maritalStatus, designation, salary, department} = req.body;
+        
+        const employee = await Employee.findById({_id: id});
+        if(!employee) {
+            return res 
+                .status(404)
+                .json({success: false, error: "employee not found"})
+        }
+        const user = await User.findById({_id: employee.userId})
+
+        if (!user) {
+            return res 
+            .status(404)
+            .json({success: false, error: "user not found"})
+        }
+
+        const updateUser = await User.findByIdAndUpdate({_id: employee.userId}, {name})
+        const updateEmployee = await Employee.findByIdAndUpdate({_id: id}, {
+            maritalStatus,
+            designation, 
+            salary,
+            department
+        })
+
+        if(!updateEmployee || !updateUser) {
+            return res 
+            .status(404)
+            .json({success: false, error: "ducument not found"})
+        }
+
+        return res.status(200).json({success: true, message: "emplooyee update"})
+
+    } catch (error) {
+        return res.status(500).json({success: false, error: "edit employee server error"})
+    }
+}
+
+export {addEmployee, upload, getEmployees, getEmployee, updateEmployee}
