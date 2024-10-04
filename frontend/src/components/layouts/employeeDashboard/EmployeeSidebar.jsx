ae80ffb245e085fbc9f0956d1dd5d097d4bb7706
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-04 10:14:09 +0700

    Merubah navlink salary untuk employee dengan menambah id

--- frontend/src/components/layouts/employeeDashboard/EmployeeSidebar.jsx
+++ frontend/src/components/layouts/employeeDashboard/EmployeeSidebar.jsx
@@ -61,7 +61,7 @@ const EmployeeSidebar = () => {
 
         {/* salary */}
         <NavLink
-          to="/employee-dashboard/salary"
+          to={`/employee-dashboard/salary/${user._id}`}
           className={({ isActive }) =>
             `${
               isActive ? "bg-teal-900" : "hover:bg-teal-800"
