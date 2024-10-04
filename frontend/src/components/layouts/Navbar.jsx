2fb345b61337c75b9da5ea5f04fae8ae14ab12cf
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-04 08:36:11 +0700

    Menambahkan handle logout pada navbar

--- frontend/src/components/layouts/Navbar.jsx
+++ frontend/src/components/layouts/Navbar.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import { useAuth } from "../../context/AuthContext";
 import Typography from "../elements/text/Typography";
 import Button from "../elements/button/Button";
+import ModalLogout from "../elements/popup/ModalLogout";
 
 const Navbar = () => {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
+
+  const handleLogout = () => {
+    ModalLogout(logout);
+  };
 
   return (
     <div className="flex items-center text-white justify-between h-12 bg-teal-900 px-5">
       <Typography> Selamat Datang, {user.name}</Typography>
-      <Button className="bg-gray-800 hover:bg-gray-700 font-bold">
+      <Button
+        className="bg-gray-800 hover:bg-gray-700 font-bold"
+        onClick={handleLogout}
+      >
         Logout
       </Button>
     </div>
