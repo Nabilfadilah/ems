3a24f07066c8a0c97dcdb1d3d45290334079cba3
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-04 08:37:28 +0700

    Membuat dashboard employee

--- frontend/src/pages/EmployeeDashboard.jsx
+++ frontend/src/pages/EmployeeDashboard.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { useAuth } from "../context/AuthContext";
+import Typography from "../components/elements/text/Typography";
+import Navbar from "../components/layouts/Navbar";
+import { Outlet } from "react-router-dom";
+import EmployeeSidebar from "../components/layouts/employeeDashboard/EmployeeSidebar";
 
 const EmployeeDashboard = () => {
-  const { user } = useAuth;
   return (
-    <div>
-      Admin Dashboard
-      {user.name}
+    <div className="flex">
+      <EmployeeSidebar />
+      <div className="flex-1 ml-64 bg-gray-100 h-screen">
+        <Navbar />
+        {/* <EmployeeSummary /> */}
+        <Outlet />
+      </div>
     </div>
   );
 };
