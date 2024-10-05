6fd55d3fd81ea4f9e51d150a4aefd1fd35d3b04c
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-06 06:21:41 +0700

    Menambahkan route get all leaves

--- server/routes/leave.js
+++ server/routes/leave.js
@@ -1,10 +1,11 @@
 import express from "express"
 import AuthMiddleware from "../middleware/AuthMiddleware.js"
-import { addLeave, getLeave } from "../controllers/LeaveController.js"
+import { addLeave, getLeave, getLeaves } from "../controllers/LeaveController.js"
 
 const router = express.Router()
 
 router.get('/:id', AuthMiddleware, getLeave)
 router.post('/add', AuthMiddleware, addLeave)
+router.get('/', AuthMiddleware, getLeaves)
 
 export default router
