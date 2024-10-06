bb75a2866457e05a234324b935dc7b5f4027cb1a
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-06 16:49:16 +0700

    Menambahkan user.role untuk url get salary

--- frontend/src/components/layouts/SalaryEmployee.jsx
+++ frontend/src/components/layouts/SalaryEmployee.jsx
@@ -3,17 +3,19 @@ import Typography from "../elements/text/Typography";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import InputSeacrh from "../elements/input/InputSearch";
+import { useAuth } from "../../context/AuthContext";
 
 const SalaryEmployee = () => {
   const [salaries, setSalaries] = useState(null);
   const [filteredSalaries, setFilteredSalaries] = useState(null);
   const { id } = useParams();
   let sno = 1;
+  const { user } = useAuth();
 
   const fetchSalaries = async () => {
     try {
       const response = await axios.get(
-        `http://localhost:5000/api/salary/${id}`,
+        `http://localhost:5000/api/salary/${id}/${user.role}`,
         {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
