75130ccaa84e50acb3a75b4bcfbf64290a7f1d6a
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-20 17:29:50 +0700

    Menambahkan class untuk buka tutup sidebar

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import AdminSidebar from "../components/layouts/AdminSidebar";
 import Navbar from "../components/layouts/Navbar";
-// import AdminSummary from "../components/layouts/AdminSummary";
 import { Outlet } from "react-router-dom";
 
 const AdminDashboard = () => {
   const { user } = useAuth;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const handleSidebarToggle = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
     <div className="flex">
-      <AdminSidebar />
-      <div className="flex-1 ml-64 bg-gray-100 h-screen">
+      {/* Pass state and handler to AdminSidebar */}
+      <AdminSidebar
+        isOpen={isSidebarOpen}
+        toggleSidebar={handleSidebarToggle}
+      />
+
+      {/* Konten Utama */}
+      <div
+        className={`flex-1 transition-all duration-300 ${
+          isSidebarOpen ? "ml-56" : "ml-20"
+        } bg-gray-100 h-screen`}
+      >
         <Navbar />
-        {/* <AdminSummary /> */}
         <Outlet />
       </div>
     </div>
