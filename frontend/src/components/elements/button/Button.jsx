83302f3b27c5d381454082607e44e13213d104c6
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-20 13:19:34 +0700

    Menambahkan props pada element button untuk disable

--- frontend/src/components/elements/button/Button.jsx
+++ frontend/src/components/elements/button/Button.jsx
@@ -5,12 +5,14 @@ const Button = (props) => {
     className = "bg-black text-white font-poppins",
     onClick = () => {},
     type = "button",
+    disabled,
   } = props;
   return (
     <button
       className={`h-7 px-4 text-sm rounded-md ${className}`}
       type={type}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
