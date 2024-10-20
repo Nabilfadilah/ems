a221f49c027a49ae11b8ee7766b081e4daa9b792
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-20 17:53:30 +0700

    Memperbaiki tampilan dashboard admin

--- frontend/src/components/layouts/AdminSummary.jsx
+++ frontend/src/components/layouts/AdminSummary.jsx
@@ -44,52 +44,52 @@ const AdminSummary = () => {
 
   return (
     <div className="p-6">
-      <h3 className="text-2xl font-bold">Dashboard Overview</h3>
+      <h3 className="text-2xl font-bold">Ringkasan Dashboard</h3>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-6">
         <SummaryCard
           icon={<FaUser />}
-          text="Total Employees"
+          text="Total Karyawan"
           number={summary.totalEmployees}
           color="bg-teal-600"
         />
         <SummaryCard
           icon={<FaBuilding />}
-          text="Total Departments"
+          text="Total Departemen"
           number={summary.totalDepartments}
           color="bg-yellow-600"
         />
         <SummaryCard
           icon={<FaMoneyBillWave />}
-          text="Monthly Salary"
+          text="Gaji Bulanan"
           number={summary.totalSalary}
           color="bg-gray-700"
         />
       </div>
 
       <div className="mt-12">
-        <h4 className="text-center text-2xl font-bold">Leave Details</h4>
+        <h4 className="text-2xl font-bold">Detail Cuti</h4>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
           <SummaryCard
             icon={<FaFileAlt />}
-            text="Leave Applied"
+            text="Cuti Diterapkan"
             number={summary.leaveSummary.appliedFor}
             color="bg-teal-700"
           />
           <SummaryCard
             icon={<FaCheckCircle />}
-            text="Leave Approved"
+            text="Cuti Disetujui"
             number={summary.leaveSummary.approved}
             color="bg-green-700"
           />
           <SummaryCard
             icon={<FaHourglassHalf />}
-            text="Leave Pending"
+            text="Cuti Tertunda"
             number={summary.leaveSummary.pending}
             color="bg-yellow-700"
           />
           <SummaryCard
             icon={<FaTimesCircle />}
-            text="Leave Rejected"
+            text="Cuti Ditolak"
             number={summary.leaveSummary.rejected}
             color="bg-red-700"
           />
