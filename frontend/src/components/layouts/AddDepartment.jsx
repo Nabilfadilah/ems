391a48d715d7a1e7d6f0b3a0990c4fc12a5f8a32
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-22 19:33:23 +0700

    Merubah tampilan form add department

--- frontend/src/components/layouts/AddDepartment.jsx
+++ frontend/src/components/layouts/AddDepartment.jsx
@@ -7,74 +7,98 @@ import { IoMdArrowBack } from "react-icons/io";
 import Label from "../elements/input/Label";
 import axios from "axios";
 import ModalAdd from "../../components/elements/popup/ModalAdd";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+
+// validasi yup
+const validationAdd = Yup.object({
+  dep_name: Yup.string().required("Nama Departemen harus diisi"),
+  description: Yup.string().required("Deskripsi harus diisi"),
+});
 
 const AddDepartment = () => {
-  // state
-  const [department, setDepartment] = useState({
-    dep_name: "",
-    description: "",
-  });
-
   const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setDepartment({ ...department, [name]: value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(
-        "http://localhost:5000/api/department/add",
-        department,
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
+  // menggunakan formik
+  const formik = useFormik({
+    initialValues: {
+      dep_name: "",
+      description: "",
+    },
+    validationSchema: validationAdd,
+    onSubmit: async (values) => {
+      try {
+        const response = await axios.post(
+          "http://localhost:5000/api/department/add",
+          values,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        ModalAdd();
+        if (response.data.success) {
+          navigate("/admin-dashboard/departments");
         }
-      );
-      ModalAdd();
-      if (response.data.success) {
-        navigate("/admin-dashboard/departments");
+      } catch (error) {
+        if (error.response && !error.response.data.success) {
+          alert(error.response.data.error);
+        }
       }
-    } catch (error) {
-      if (error.response && !error.response.data.success) {
-        alert(error.response.data.error);
-      }
-    }
-  };
+    },
+  });
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setDepartment({ ...department, [name]: value });
+  // };
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  // };
 
   return (
-    <div className="max-w-6xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md w-full">
+    <div className="max-w-6xl mx-auto bg-white p-8 rounded-md shadow-2xl w-full">
       <div className="flex justify-between items-center mb-6">
-        <Typography className="text-xl font-bold">Add Department</Typography>
+        <Typography className="text-xl font-bold">Tambah Departemen</Typography>
         <Link to="/admin-dashboard/departments">
-          <Button className="bg-gray-700 hover:bg-gray-600 text-white flex items-center gap-1">
-            <IoMdArrowBack strokeWidth={2} className="h-4 w-4" /> Back
+          <Button className="bg-gray-700 hover:bg-gray-600 text-white flex items-center gap-1 shadow-xl font-bold">
+            <IoMdArrowBack strokeWidth={2} className="h-4 w-4" /> Kembali
           </Button>
         </Link>
       </div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <div className="w-full">
           <InputForm
-            className="w-full"
+            className={`w-full ${
+              formik.touched.dep_name && formik.errors.dep_name
+                ? "border-red-500"
+                : "border-gray-400"
+            }`}
             name="dep_name"
-            label="Department Name"
+            label="Nama Departemen"
             type="text"
             placeholder="John Doe"
-            required
-            onChange={handleChange}
+            value={formik.values.dep_name}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
         </div>
 
         <div className="w-full mt-5">
-          <Label>Description</Label>
+          <Label>Deskripsi</Label>
           <textarea
             name="description"
             placeholder="John Doe"
-            className="mt-1 p-2 block w-full border border-gray-400 rounded-md"
-            onChange={handleChange}
+            className={`mt-1 p-2 block w-full border rounded-md ${
+              formik.touched.description && formik.errors.description
+                ? "border-red-500"
+                : "border-gray-400"
+            }`}
+            value={formik.values.description}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
         </div>
 
@@ -82,7 +106,7 @@ const AddDepartment = () => {
           type="submit"
           className="bg-green-700 hover:bg-green-600 w-full font-bold text-white h-8 mt-5"
         >
-          Add Department
+          Simpan Data
         </Button>
       </form>
     </div>
