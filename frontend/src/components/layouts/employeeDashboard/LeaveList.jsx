00dad83f7428bdb8c177e9f9f34af96347177b72
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-22 16:44:36 +0700

    Merubah tampilan leave dan merubah tabelnya serta menambah button back untuk admin

--- frontend/src/components/layouts/employeeDashboard/LeaveList.jsx
+++ frontend/src/components/layouts/employeeDashboard/LeaveList.jsx
@@ -6,6 +6,8 @@ import { Link, useParams } from "react-router-dom";
 import DataTable from "react-data-table-component";
 import axios from "axios";
 import { useAuth } from "../../../context/AuthContext";
+import { FaRegPlusSquare } from "react-icons/fa";
+import { IoMdArrowBack } from "react-icons/io";
 
 const LeaveList = () => {
   const [leaves, setleaves] = useState([]);
@@ -57,43 +59,52 @@ const LeaveList = () => {
       {filteredleaves === null ? (
         <div>Loading.....</div>
       ) : (
-        <div className="p-6">
+        <div className="leave">
           <div className="text-center">
-            <Typography className="text-2xl font-bold">Manage leave</Typography>
+            <Typography className="text-2xl font-bold">Kelola Cuti</Typography>
           </div>
           <div className="flex justify-between items-center">
             <InputSeacrh
-              placeholder="Seacrh By Dep Name"
+              placeholder="Cari berdasarkan jenis cuti"
               onChange={handleSearch}
             />
 
             {user.role === "employee" && ( // Kondisi untuk hanya menampilkan button jika user adalah admin
-              <Button className="px-4 py-1 bg-green-800 hover:bg-green-700 text-white font-bold">
-                <Link to="/employee-dashboard/add-leave">Add New</Link>
-              </Button>
+              <Link to="/employee-dashboard/add-leave">
+                <Button className="px-4 py-1 bg-green-800 hover:bg-green-700 text-white font-bold">
+                  <FaRegPlusSquare strokeWidth={2} className="h-4 w-4" /> Tambah
+                </Button>
+              </Link>
+            )}
+
+            {user.role === "admin" && (
+              <Link to="/admin-dashboard/employees">
+                <Button className="bg-gray-700 hover:bg-gray-600 text-white flex items-center gap-1 shadow-xl font-bold">
+                  <IoMdArrowBack strokeWidth={2} className="h-4 w-4" /> Kembali
+                </Button>
+              </Link>
             )}
           </div>
 
           <div className="mt-5">
-            <table className="w-full text-sm text-left text-gray-500">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 border border-gray-200">
+            <table className="w-full text-sm text-left text-gray-500 shadow-2xl mt-5">
+              <thead className="text-xs text-white uppercase bg-gray-800 border border-gray-700">
                 <tr>
-                  <th className="px-6 py-3">SNO</th>
-                  <th className="px-6 py-3">leave Type</th>
-                  <th className="px-6 py-3">From</th>
-                  <th className="px-6 py-3">To</th>
-                  <th className="px-6 py-3">Description</th>
+                  <th className="px-6 py-3">No.</th>
+                  <th className="px-6 py-3">Jenis Cuti</th>
+                  <th className="px-6 py-3">Dari</th>
+                  <th className="px-6 py-3">Sampai</th>
+                  <th className="px-6 py-3">Deskripsi</th>
                   <th className="px-6 py-3">Status</th>
                 </tr>
               </thead>
-              <tbody className="text-white">
-                {filteredleaves.map((leave) => (
+              <tbody className="text-black shadow-2xl">
+                {filteredleaves.map((leave, index) => (
                   <tr
-                    // key={leave.id}
                     key={leave._id}
-                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                    className="bg-white border-b dark:bg-white dark:border-gray-300"
                   >
-                    <td className="px-6 py-3">{sno++}.</td>
+                    <td className="px-6 py-3">{index + 1}.</td>
                     <td className="px-6 py-3">{leave.leaveType}</td>
                     <td className="px-6 py-3">
                       {new Date(leave.startDate).toLocaleDateString()}
@@ -102,7 +113,19 @@ const LeaveList = () => {
                       {new Date(leave.endDate).toLocaleDateString()}
                     </td>
                     <td className="px-6 py-3">{leave.reason}</td>
-                    <td className="px-6 py-3">{leave.status}</td>
+                    <td
+                      className={`px-6 py-3 font-extrabold ${
+                        leave.status === "Approved"
+                          ? "text-green-500"
+                          : leave.status === "Pending"
+                          ? "text-yellow-500"
+                          : leave.status === "Rejected"
+                          ? "text-red-500"
+                          : ""
+                      }`}
+                    >
+                      {leave.status}
+                    </td>
                     {/* <td className="px-6 py-3">
                       {new Date(leave.appliedDate).toLocaleDateString()}
                     </td> */}
