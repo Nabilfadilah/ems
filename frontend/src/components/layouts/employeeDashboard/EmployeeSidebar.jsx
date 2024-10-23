422a83d8d029e7e86cfc181cb2fd8ad2d980fe3e
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-23 15:04:53 +0700

    Memperbaiki sidebar employee agar dapat diatur kecil atau besar

--- frontend/src/components/layouts/employeeDashboard/EmployeeSidebar.jsx
+++ frontend/src/components/layouts/employeeDashboard/EmployeeSidebar.jsx
@@ -4,33 +4,56 @@ import {
   FaBuilding,
   FaCalendarAlt,
   FaCogs,
-  FaMoneyBillWave,
   FaTachometerAlt,
+  FaMoneyBillWave,
   FaUser,
+  FaChevronLeft,
+  FaChevronRight,
 } from "react-icons/fa";
 import { useAuth } from "../../../context/AuthContext";
+import emsLogo from "../../../assets/img/emsLogo.png";
 
-const EmployeeSidebar = () => {
+const EmployeeSidebar = ({ isOpen, toggleSidebar }) => {
   const { user } = useAuth();
 
   return (
-    <div className="bg-gray-800 text-white h-screen fixed left-0 top-0 bottom-0 space-y-2 w-64">
-      <div className="bg-teal-900 h-12 flex items-center justify-center">
-        <Typography className="text-2xl text-center">Employee MS</Typography>
+    <div
+      className={`${
+        isOpen ? "w-56" : "w-20"
+      } bg-cyan-950 text-white h-screen fixed top-0 bottom-0 transition-all duration-300 rounded-e-xl`}
+    >
+      <div className="bg-cyan-950 h-20 flex items-center justify-between px-5">
+        <img
+          src={emsLogo}
+          className={`cursor-pointer duration-500 w-10 h-10 ${
+            open && "rotate-[360deg]"
+          }`}
+        />
+        <Typography
+          className={`${isOpen ? "text-lg" : "hidden"} text-center font-bold`}
+        >
+          Managemen Karyawan
+        </Typography>
+        <button onClick={toggleSidebar} className="text-white">
+          {isOpen ? <FaChevronLeft /> : <FaChevronRight />}
+        </button>
       </div>
-      <div className="px-4">
+
+      <div className="px-4 mt-4">
         {/* dashboard */}
         <NavLink
           to="/employee-dashboard"
           className={({ isActive }) =>
             `${
-              isActive ? "bg-teal-900 " : "hover:bg-teal-800"
-            } flex items-center space-x-4 py-2.5 px-4 rounded`
+              isActive ? "bg-cyan-900" : "hover:bg-cyan-800"
+            } flex items-center space-x-4 py-2.5 px-4 rounded transition-all duration-200 mb-2`
           }
           end
         >
           <FaTachometerAlt />
-          <Typography>Dashboard</Typography>
+          <Typography className={`${!isOpen && "hidden"} text-sm`}>
+            Dashboard
+          </Typography>
         </NavLink>
 
         {/* profile */}
@@ -38,12 +61,14 @@ const EmployeeSidebar = () => {
           to={`/employee-dashboard/profile/${user._id}`}
           className={({ isActive }) =>
             `${
-              isActive ? "bg-teal-900" : "hover:bg-teal-800"
-            } flex items-center space-x-4 py-2.5 px-4 rounded`
+              isActive ? "bg-cyan-900" : "hover:bg-cyan-800"
+            } flex items-center space-x-4 py-2.5 px-4 rounded transition-all duration-200 mb-2`
           }
         >
           <FaUser />
-          <Typography>My Profile</Typography>
+          <Typography className={`${!isOpen && "hidden"} text-sm`}>
+            Profil Saya
+          </Typography>
         </NavLink>
 
         {/* leaves */}
@@ -51,12 +76,14 @@ const EmployeeSidebar = () => {
           to={`/employee-dashboard/leaves/${user._id}`}
           className={({ isActive }) =>
             `${
-              isActive ? "bg-teal-900" : "hover:bg-teal-800"
-            } flex items-center space-x-4 py-2.5 px-4 rounded`
+              isActive ? "bg-cyan-900" : "hover:bg-cyan-800"
+            } flex items-center space-x-4 py-2.5 px-4 rounded transition-all duration-200 mb-2`
           }
         >
           <FaBuilding />
-          <Typography>Leaves</Typography>
+          <Typography className={`${!isOpen && "hidden"} text-sm`}>
+            Cuti
+          </Typography>
         </NavLink>
 
         {/* salary */}
@@ -64,12 +91,14 @@ const EmployeeSidebar = () => {
           to={`/employee-dashboard/salary/${user._id}`}
           className={({ isActive }) =>
             `${
-              isActive ? "bg-teal-900" : "hover:bg-teal-800"
-            } flex items-center space-x-4 py-2.5 px-4 rounded`
+              isActive ? "bg-cyan-900" : "hover:bg-cyan-800"
+            } flex items-center space-x-4 py-2.5 px-4 rounded transition-all duration-200 mb-2`
           }
         >
-          <FaCalendarAlt />
-          <Typography>Salary</Typography>
+          <FaMoneyBillWave />
+          <Typography className={`${!isOpen && "hidden"} text-sm`}>
+            Gaji
+          </Typography>
         </NavLink>
 
         {/* settings */}
@@ -77,12 +106,14 @@ const EmployeeSidebar = () => {
           to="/employee-dashboard/setting"
           className={({ isActive }) =>
             `${
-              isActive ? "bg-teal-900" : "hover:bg-teal-800"
-            } flex items-center space-x-4 py-2.5 px-4 rounded`
+              isActive ? "bg-cyan-900" : "hover:bg-cyan-800"
+            } flex items-center space-x-4 py-2.5 px-4 rounded transition-all duration-200 mb-2`
           }
         >
           <FaCogs />
-          <Typography>Settings</Typography>
+          <Typography className={`${!isOpen && "hidden"} text-sm`}>
+            Pengaturan
+          </Typography>
         </NavLink>
       </div>
     </div>
