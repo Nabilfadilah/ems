15060bd81f04e1824eea8b7135b54ff8d23cf067
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-23 15:05:14 +0700

    Menambahkan class untuk buka tutup sidebar employee

--- frontend/src/pages/EmployeeDashboard.jsx
+++ frontend/src/pages/EmployeeDashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import Typography from "../components/elements/text/Typography";
 import Navbar from "../components/layouts/Navbar";
@@ -6,13 +6,27 @@ import { Outlet } from "react-router-dom";
 import EmployeeSidebar from "../components/layouts/employeeDashboard/EmployeeSidebar";
 
 const EmployeeDashboard = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const handleSidebarToggle = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="flex">
-      <EmployeeSidebar />
-      <div className="flex-1 ml-64 bg-gray-100 h-screen">
+    <div className="flex h-screen overflow-hidden">
+      <EmployeeSidebar
+        isOpen={isSidebarOpen}
+        toggleSidebar={handleSidebarToggle}
+      />
+      <div
+        className={`flex-1 flex flex-col transition-all duration-300 ${
+          isSidebarOpen ? "ml-56" : "ml-20"
+        } bg-gray-100 overflow-y-auto`}
+      >
         <Navbar />
-        {/* <EmployeeSummary /> */}
-        <Outlet />
+        <div className="flex-1 overflow-x-auto p-5">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
